3bd34374bb00f6a228b2e239839752e789a5d67e
GaryN56 <garynguyen2317@gmail.com>
2025-03-15 18:18:12 -0700

    Updated AppFactory
    Added a makeEditCommand method

--- mvcApps/src/mvc/AppFactory.java
+++ mvcApps/src/mvc/AppFactory.java
@@ -4,6 +4,7 @@ public interface AppFactory {
     public Model makeModel();
     public View makeView(Model m);
     public String[] getEditCommands();
+    public Command makeEditCommand(Model model, String type, Object source);
     public String getTitle();
     public String[] getHelp();
     public String about();
