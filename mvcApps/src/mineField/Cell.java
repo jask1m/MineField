7b91fb25ee79dcdf7214bfa51996e1be4eb31e97
jask1m <jkim94960@gmail.com>
2025-03-18 20:17:28 -0700

    implement Cell class

---
+++ mvcApps/src/mineField/Cell.java
@@ -0,0 +1,37 @@
+package mineField;
+
+import java.io.Serializable;
+
+public class Cell implements Serializable {
+    private final boolean isMine;
+    private boolean hasBeenMined;
+    private String label;
+    private final int numMinesNearby;
+
+    public Cell(boolean isMine, boolean hasBeenMined, String label, int numMinesNearby) {
+        this.isMine = isMine;
+        this.hasBeenMined = hasBeenMined;
+        this.label = label;
+        this.numMinesNearby = numMinesNearby;
+    }
+
+    public void setHasBeenMined(boolean hasBeenMined) {
+        this.hasBeenMined = hasBeenMined;
+    }
+
+    public boolean getHasBeenMined() {
+        return hasBeenMined;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getNumMinesNearby() {
+        return numMinesNearby;
+    }
+}
